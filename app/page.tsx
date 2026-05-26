bf8ba6a2900378ab6ffd2adf868a65ebdd5d6c20
Ricardo <jreigericardo1@gmail.com>
2026-05-26 13:50:51 -0300

    Atualização do ecommerce Hort

--- app/page.tsx
+++ app/page.tsx
@@ -6,22 +6,45 @@ import { FaWhatsapp, FaShoppingCart } from "react-icons/fa";
 export default function Home() {
   const [cart, setCart] = useState<string[]>([]);
 const [quantities, setQuantities] = useState<{ [key: string]: string }>({});
-
+const phoneNumber = "5511913689013";
+const productsPrice: { [key: string]: number } = {
+  "Maçã Fuji": 16.99,
+  "Banana Nanica": 8.99,
+  "Alface Americana": 4.99,
+  "Tomate": 12.99,
+};
 const addToCart = (produto: string) => {
-  setCart([...cart, produto]);
+  const quantidade = quantities[produto];
+
+  if (!quantidade) {
+    alert("Digite a quantidade antes de adicionar.");
+    return;
+  }
+
+  setCart([...cart, `${produto} - ${quantidade}`]);
+  setQuantities({
+  ...quantities,
+  [produto]: "",
+});
 };
 
 const cartMessage = encodeURIComponent(
-  `Olá, gostaria de fazer esse pedido:\n\n${cart
+  `Olá, gostaria de fazer um pedido:\n\n${cart
     .map((item, index) => `${index + 1}. ${item}`)
     .join("\n")}`
 );
- return (
+
+const total = cart.reduce((acc, item) => {
+  const nome = item.split(" - ")[0];
+  return acc + (productsPrice[nome] || 0);
+}, 0);
+
+return (
   <main className="bg-black text-white min-h-screen overflow-hidden">
       {/* NAVBAR */}
-     <nav className="fixed top-0 left-0 w-full z-50 backdrop-blur-md bg-black/30 border-b border-white/10">
+     <div className="fixed top-0 left-0 w-full z-50 bg-black/80 backdrop-blur-md border-b border-white/10">
 
-  <div className="flex items-center justify-between px-10 py-6">
+  <div className="flex items-center justify-between px-4 md:px-10 py-4 md:py-6">
 
   
 
@@ -32,7 +55,7 @@ const cartMessage = encodeURIComponent(
       className="w-10 h-10 object-contain rounded-full"
     />
 
-    <h1 className="text-2xl tracking-[0.4em] font-bold">
+    <h1 className="text-2xl tracking-[0.6em] font-bold">
       HORT
     </h1>
   </div>
@@ -60,7 +83,7 @@ Contato
 </div>
 
 <a
-  href="https://wa.me/5511913689013?text=Olá,%20gostaria%20de%20fazer%20um%20pedido!"
+ href={`https://wa.me/${phoneNumber}?text=${cartMessage}`}
   target="_blank"
   className="md:hidden bg-green-500 text-black px-4 py-2 rounded-full text-sm font-bold"
 >
@@ -69,7 +92,7 @@ Contato
 
 </div>
 
-</nav>
+</div>
 
       {/* HERO */}
       <motion.section
@@ -78,19 +101,28 @@ Contato
   transition={{ duration: 1.2 }}
   className="flex flex-col items-center justify-center text-center px-6 pt-32"
 >
-
-        <h1 className="text-7xl md:text-8xl font-bold tracking-[0.3em]">
-          HORT LOURENÇO
-        </h1>
-
+<motion.div
+  initial={{ opacity: 0, y: 40 }}
+  animate={{ opacity: 1, y: 0 }}
+  transition={{ duration: 1 }}
+>
+  <Image
+    src="/images/bannerhort.png"
+    alt="Hort Lourenço"
+    width={1600}
+    height={900}
+    className="w-[95%] md:w-[80%] max-h-[700px] h-auto mx-auto rounded-2xl md:rounded-3xl shadow-2xl mt-24 md:mt-8 object-cover"
+  />
+</motion.div>
+<div className="h-10"></div>
         <p className="text-zinc-400 mt-8 max-w-xl text-lg">
           Frutas frescas, verduras selecionadas e qualidade premium para sua família.
         </p>
-
-   <a
-  href="https://wa.me/5511913689013?text=Olá,%20gostaria%20de%20fazer%20um%20pedido!"
+<a
+ href={`https://wa.me/${phoneNumber}?text=${cartMessage}`}
   target="_blank"
-  className="mt-10 px-10 py-5 bg-green-500 rounded-full text-black font-bold hover:scale-110 hover:bg-green-400 transition-all duration-500 shadow-[0_0_40px_rgba(34,197,94,0.5)]"
+  rel="noopener noreferrer"
+  className="mt-10 px-10 py-5 bg-green-500 rounded-full text-black font-bold hover:scale-110 transition"
 >
   Fazer Pedido
 </a>
@@ -103,9 +135,9 @@ Contato
   whileInView={{ opacity: 1, y: 0 }}
   transition={{ duration: 1 }}
   viewport={{ once: true }}
-  className="relative grid md:grid-cols-3 gap-8 px-10 py-32"
+  className="relative grid md:grid-cols-1 md:grid-cols-3 gap-8 px-10 py-16"
 >
-        <div className="relative z-10 bg-white/5 border border-white/10 rounded-3xl overflow-hidden backdrop-blur-md hover:-translate-y-4 hover:shadow-[0_0_60px_rgba(34,197,94,0.7)] animate-pulse transition-all duration-500">
+       <div className="relative z-10 bg-white/5 border border-white/10 rounded-3xl overflow-hidden hover:scale-105 transition-all duration-300">
           <Image src="/images/frutas.jpg" alt="Frutas" width={500} height={300} className="w-full h-52 object-cover hover:scale-110 transition-all duration-700" />
           <div className="p-8">
             <h2 className="text-3xl mb-4">🍎 Frutas</h2>
@@ -118,7 +150,7 @@ Contato
           </div>
         </div>
 
-       <div className="relative z-10 bg-white/5 border border-white/10 rounded-3xl overflow-hidden backdrop-blur-md hover:-translate-y-4 hover:shadow-[0_0_60px_rgba(34,197,94,0.7)] animate-pulse transition-all duration-500">
+       <div className="relative z-10 bg-white/5 border border-white/10 rounded-3xl overflow-hidden hover:scale-105 transition-all duration-300">
           <Image src="/images/legumes.jpg" alt="Legumes" width={500} height={300} className="w-full h-52 object-cover hover:scale-110 transition-all duration-700" />
           <div className="p-8">
             <h2 className="text-3xl mb-4">🥬 Legumes</h2>
@@ -131,7 +163,7 @@ Contato
           </div>
         </div>
 
-        <div className="relative z-10  bg-white/5 border border-white/10 rounded-3xl overflow-hidden backdrop-blur-md hover:-translate-y-4 hover:shadow-[0_0_60px_rgba(34,197,94,0.7)] animate-pulse transition-all duration-500">
+       <div className="relative z-10 bg-white/5 border border-white/10 rounded-3xl overflow-hidden hover:scale-105 transition-all duration-300">
           <Image src="/images/delivery.jpg" alt="Delivery" width={500} height={300} className="w-full h-52 object-cover hover:scale-110 transition-all duration-700" />
           <div className="p-8">
             <h2 className="text-3xl mb-4">🚚 Delivery</h2>
@@ -157,7 +189,8 @@ Contato
 >
  <div className="relative overflow-hidden rounded-[40px] border border-green-500/20 bg-gradient-to-r from-green-500/10 to-black p-10 max-w-7xl mx-auto">
 
-    <div className="absolute inset-0 bg-green-500/5 blur-3xl"></div>
+  
+
 
     <div className="relative z-10 grid grid-cols-1 md:grid-cols-2 items-center gap-10">
 
@@ -278,7 +311,7 @@ Contato
     Produtos em destaque
   </h2>
 
-  <div className="grid grid-cols-1 md:grid-cols-4 gap-6 max-w-7xl mx-auto">
+  <div className="grid grid-cols-2 md:grid-cols-4 gap-6 max-w-7xl mx-auto">
     {[
       ["🍎", "Maçã Fuji", "R$16,99", "/kg"],
       ["🍌", "Banana Nanica", "R$8,99", "/kg"],
@@ -287,7 +320,7 @@ Contato
     ].map(([emoji, nome, preco, unidade]) => (
       <div
         key={nome}
-        className="bg-white/5 border border-white/10 rounded-3xl p-6 hover:-translate-y-3 hover:border-green-500 hover:shadow-[0_0_50px_rgba(34,197,94,0.25)] transition-all duration-500"
+        className="bg-zinc-900/80 border border-white/10 rounded-3xl p-6 hover:-translate-y-3 hover:border-green-500 hover:shadow-[0_0_50px_rgba(34,197,94,0.25)] transition-all duration-500"
       >
         <div className="text-5xl mb-6">{emoji}</div>
 
@@ -298,26 +331,24 @@ Contato
           <span className="text-zinc-500">{unidade}</span>
         </div>
 
-        <button
-  onClick={() =>
-  addToCart(
-    `${nome} - ${quantities[nome] || "quantidade não informada"} - ${preco}${unidade}`
-  )
-}
-  className="inline-block bg-green-500 hover:bg-green-400 text-black px-5 py-3 rounded-full font-bold transition-all duration-300"
+       <button
+ onClick={() => addToCart(nome)}
+  className="inline-block bg-green-400 hover:bg-green-300 text-black px-5 py-3 rounded-2xl"
 >
   Adicionar
-  <input
+</button>
+
+<input
   type="text"
   placeholder={unidade === "/kg" ? "Ex: 300g ou 1kg" : "Ex: 1 un"}
   value={quantities[nome] || ""}
   onChange={(e) =>
     setQuantities({ ...quantities, [nome]: e.target.value })
   }
-  className="w-full mb-4 bg-black/40 border border-white/10 rounded-2xl px-4 py-3 text-sm outline-none focus:border-green-500"
+  className="w-full mt-4 bg-black/80 border border-white/10 rounded-2xl px-4 py-3"
 />
 
-</button>
+
       </div>
     ))}
   </div>
@@ -328,6 +359,9 @@ Contato
       <FaShoppingCart className="text-green-400" />
       <h3 className="font-bold text-xl">
   Carrinho ({cart.length})
+  <p className="text-green-400 font-bold text-lg">
+  Total: R${total.toFixed(2)}
+</p>
 </h3>
     </div>
 
@@ -352,7 +386,7 @@ Contato
 </button>
   </div>
 )}
-<footer className="border-t border-white/10 mt-32 py-10 text-center text-zinc-500">
+<footer className="border-t border-white/10 mt-16 py-10 text-center text-zinc-500">
 
   <h2 className="text-2xl tracking-[0.3em] text-white mb-4">
     HORT LOURENÇO
@@ -367,6 +401,58 @@ Contato
     © 2026 HORT Lourenço. Todos os direitos reservados.
   </p>
 
+</footer>
+<footer className="border-t border-white/10 mt-32 bg-black/80">
+  <div className="max-w-7xl mx-auto px-6 py-12 grid grid-cols-1 md:grid-cols-4 gap-10">
+
+    <div>
+      <h2 className="text-2xl tracking-[0.5em] font-bold mb-4">
+        HORT
+      </h2>
+      <p className="text-zinc-400">
+        Frutas, verduras e legumes selecionados com qualidade premium.
+      </p>
+    </div>
+
+    <div>
+      <h3 className="text-lg font-semibold mb-4 text-green-400">
+        Contato
+      </h3>
+
+      <p className="text-zinc-400">(11) 91368-9013</p>
+      <p className="text-zinc-400">contato@hort.com</p>
+    </div>
+
+    <div>
+      <h3 className="text-lg font-semibold mb-4 text-green-400">
+        Funcionamento
+      </h3>
+
+      <p className="text-zinc-400">Segunda a Sábado</p>
+      <p className="text-zinc-400">07h às 20h</p>
+      <p className="text-zinc-400">Domingo: 07h as 13h</p>
+    </div>
+
+    <div>
+      <h3 className="text-lg font-semibold mb-4 text-green-400">
+        Redes
+      </h3>
+
+      <div className="flex gap-4">
+        <a href="#" className="text-zinc-400 hover:text-green-400 transition">
+          Instagram
+        </a>
+
+        <a href="#" className="text-zinc-400 hover:text-green-400 transition">
+          WhatsApp
+        </a>
+      </div>
+    </div>
+  </div>
+
+  <div className="border-t border-white/10 py-6 text-center text-zinc-500 text-sm">
+    
+  </div>
 </footer>
     </main>
   );
